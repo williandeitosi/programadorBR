630421bbb682f79cb4e8cf8bf447ae8b03fff179
Willian Giovanini Dei Tosi <104787632+williandeitosi@users.noreply.github.com>
2022-05-23 22:21:33 -0300

    basico

---
+++ JavaScript/HelloWorld/script.js
@@ -0,0 +1,12 @@
+var idade = 9
+
+var menor10 = idade <= 10 // true
+
+var maior65 = idade >= 65 // true
+
+var gratuidade = menor10 || maior65
+
+console.log("Minha idade é =", idade)
+console.log("Sou meior de 65 ?", maior65)
+console.log("Sou menor de 10 ?", menor10)
+console.log("Tenho gratuidade ?",gratuidade)
