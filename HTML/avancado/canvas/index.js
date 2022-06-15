870ffc5d464e35cd2ba8345f5348ffd7f58a45ed
Willian Giovanini Dei Tosi <104787632+williandeitosi@users.noreply.github.com>
2022-06-15 14:41:28 -0300

    canvas2

--- HTML/avancado/canvas/index.js
+++ HTML/avancado/canvas/index.js
@@ -1,20 +1,65 @@
 
 
-let jogador = document.getElementById("jogador")
+let tela = document.getElementById("tela")
 
-let xInicial = 0
-let yInicial = 0
+let ctx = tela.getContext("2d")
 
-function moverJogadorPara(x, y) {
+/* desenhar uma linha
 
-    let posX = x + "px"
-    let posY = y + "px"
+ctx.moveTo(0,0)
+ctx.lineWidth = 5
+ctx.strokeStyle = "green"
+ctx.lineTo(250,250)
+ctx.lineTo(500,0)
+ctx.stroke()
 
-    jogador.style.top = posX
-    jogador.style.left = posY
-}
+*/
 
-setInterval(function(){
 
-    moverJogadorPara(xInicial++, yInicial++)
-}, 100)
+/* Retangulo preenchido
+
+ctx.fillStyle = "green"
+ctx.fillRect(10, 10, 100, 200)
+
+*/
+
+
+/*  Apenas o contorno 
+ctx.strokeStyle = "blue"
+ctx.strokeRect(10, 10, 100, 200)
+*/
+
+
+
+/*  Retangulo com borda de cor diferente
+ctx.rect(10, 10, 100, 200)
+ctx.fillStyle = "blue"
+ctx.strokeStyle = "green"
+ctx.lineWidth = 5
+ctx.fill()
+ctx.stroke()
+ctx.clearRect (20,20,30,30)  */
+
+
+
+
+ctx.beginPath() /* começa uma nova linha */
+
+ctx.lineWidth = 4
+ctx.strokeStyle = "red"
+ctx.moveTo(10, 10)
+ctx.lineTo(400, 300)
+ctx.stroke()
+
+
+ctx.beginPath()
+
+ctx.lineWidth = 7
+ctx.strokeStyle = "blue"
+ctx.moveTo(10, 50)
+ctx.lineTo(200, 300)
+ctx.lineTo(100, 300)
+ctx.closePath() /* fecha altomaticamente as linhas  */
+ctx.fillStyle = "yellow"
+ctx.stroke()
+ctx.fill()
