420ab11e6aded6d4db9d14c28a7f981d58eb21ee
Willian Giovanini Dei Tosi <104787632+williandeitosi@users.noreply.github.com>
2022-06-27 22:22:40 -0300

    continuacao

--- JavaScript/avancado/desafioJSavancado/index.js
+++ JavaScript/avancado/desafioJSavancado/index.js
@@ -1,18 +1,56 @@
 
-
-function novoAluno (nome, idade) {
-   return {nome, idade}
-}
-
-let alunos = [
-   novoAluno("Willian", 26),
-   novoAluno("Rafaaela", 20),
-   novoAluno("Maria", 35),
-   novoAluno("Celia", 45),
+// REDUCE
+const pokemons = [
+   {
+      nome: "Pikachu",
+      tipo: "eletrico"
+   },
+   {
+      nome:"Squirtle",
+      tipo:"agua"
+   },
+   {
+      nome:"Magikarp",
+      tipo:"agua"
+   },
 ]
 
-function idadeDaTurma (total, aluno) {
-   return total + idade.aluno
-}
+const pokemonsPorTipo = pokemons.reduce((pokemonsPorTipo, pokemonAtual) => {
+   console.log(pokemonAtual)
 
-console.log(alunos.reduce(idadeDaTurma,0))
+   pokemonsPorTipo[pokemonAtual.tipo] = pokemonsPorTipo[pokemonAtual.tipo] || []
+
+   pokemonsPorTipo[pokemonAtual.tipo].push(pokemonAtual)
+
+   return pokemonsPorTipo
+},{})
+
+console.log(pokemonsPorTipo.agua)
+
+
+// MAP
+
+
+const produtos = [
+   {
+      prod: "notebook",
+      preco: 2000.00
+   },
+   {
+      prod: "smartphone",
+      preco: 1550.00
+   }
+]
+
+function precoEmReal (p) {
+   return p.preco * 2
+} 
+
+let resultado = produtos.map(precoEmReal)
+console.log(resultado)
+
+// let precoEmReal = produtos.map((p) => {
+//    return p.preco * 4
+   
+// })
+
